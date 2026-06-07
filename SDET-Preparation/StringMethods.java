97c933e3b16ecbbfeb225ef2915163f84080c492
Sakshi Shukla <sakshishukla@Sakshis-MacBook-Air.local>
2026-06-08 01:17:05 +0530

    Day 4 StringProblem2 and StringMethods

---
+++ SDET-Preparation/StringMethods.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class StringMethods {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String str = "automation";
+        System.out.println(str.charAt(1));
+        System.out.println(str.substring(1,5));
+        String st="rich";
+        if(str.contains(st))
+        {
+            System.out.println("It's a great day");
+        }
+        else
+        {
+            System.out.println("It's a great night");
+        }
+        System.out.println(str.replace(str.substring(1,5), st));
+        System.out.println(str.trim());
+    }
+}
---
+++ SDET-Preparation/StringProblems2.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class StringProblems2 {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String st1=sc.nextLine();
+        int c=0;
+        String st=st1.toLowerCase();
+        for(int i=0;i<st.length();i++)
+        {
+            if(st.charAt(i)!='a'&& st.charAt(i)!='e'&& st.charAt(i)!='i'&& st.charAt(i)!='o'&& st.charAt(i)!='u')
+            {
+                c++;
+            }
+        }
+        System.out.println(c);
+        
+
+        //anagram sol
+
+        String st1="aab";
+        String st2="abb";
+        int a=st1.length();
+        int b=st2.length();
+        boolean[] visited = new boolean[st2.length()];
+        int count=0;
+        if(a==b)
+        {       
+            for(int i=0;i<a;i++)
+            {   
+                for(int j=0;j<b;j++)
+                {
+                    if(!visited[j] && st1.charAt(i)==(st2.charAt(j)))
+                    {
+                        visited[j]=true;
+                        count++;
+                        break;
+                    }
+                }
+            }
+        }
+        if(count==a)
+        {
+            System.out.println("anagram");
+        }
+        else
+        {
+            System.out.println("not anagram");
+        }
+
+
+    }
+}
